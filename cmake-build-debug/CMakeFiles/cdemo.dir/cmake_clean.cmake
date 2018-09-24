5922f470bfd46ade6f30d39a93bb213fb8894625
HungLVTH1809008 <1621050375@student.humg.edu.vn>
2018-09-24 20:27:35 +0700

    bai dau tien

---
+++ cmake-build-debug/CMakeFiles/cdemo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cdemo.dir/main.c.obj"
+  "cdemo.pdb"
+  "cdemo.exe"
+  "cdemo.exe.manifest"
+  "libcdemo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cdemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
